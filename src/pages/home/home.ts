24e306e5bcf7f81b08eb774048dd059cf088ce41
SA <shivaram@gpsl.co>
2018-05-08 18:43:03 +0530

    in app browser added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private inAppBrowser: InAppBrowser) {
+    const options:InAppBrowserOptions = {
+      zoom : 'no'
+    };
+    const browser = this.inAppBrowser.create('http://beautycareperfumes.com', '_self', options);
+    browser.show();
   }
-
+  
 }
